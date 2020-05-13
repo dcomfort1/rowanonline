052aaf9e809f5bc5c0cc2f388fa7788c6b21eb21
Monteleone-Haught <cmontele@rcsj.edu>
2020-05-13 14:47:11 -0400

    Working on the theme for the tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      wl: "1500px"
+    },
+    extend: {
+      colors: {
+        'navy': '#005494',
+        'gold': '#FFCC00',
+      },
+    },
   },
   variants: {},
   plugins: [],
