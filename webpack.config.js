d509776e4ba1e13c04d4024d761544368770261f
Cory Monteleone <cmontele@rcsj.edu>
2020-05-20 16:22:32 -0400

    Config was added. Need to have package changed around a bit and fronted with a gulp task.

---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    keep_fnames: false,
+                },
+            }),
+        ],
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false, 
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [
+                                require('autoprefixer'),
+                                require('cssnano')({
+                                    preset: 'default',
+                                })
+                            ],
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false, 
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [
+                                require('tailwindcss'),
+                                require('autoprefixer'),
+                                require('cssnano')({
+                                    preset: 'default',
+                                })
+                            ],
+                            sourceMap: true
+                        }
+                    }
+                ],
+            },
+            // {
+            //     test: /\.m?js$/,
+            //     exclude: /(node_modules|bower_components)/,
+            //     use: {
+            //         loader: 'babel-loader',
+            //         options: {
+            //             presets: ['@babel/preset-env']
+            //         }
+            //     }
+            // }
+        ],
+    },
+    entry: ["./src/js/main.js", "./src/css/tailwind.css"],
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: 'bundled.css',
+        }),
+      ],
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'public/build/js'),
+    },
+};
