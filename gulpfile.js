f89548788b508e86b88bf3d4a43ee2487f5c14ae
Cory Monteleone <cmontele@rcgc.edu>
2021-02-19 08:41:43 -0500

    Sitemap functionality now added

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const webpack = require('webpack')
 const webpackConfig = require('./webpack.config.js')
 const nunjucks = require('gulp-nunjucks-render')
 const gulp = require('gulp')
+var sitemap = require('gulp-sitemap')
 
 function assets(cb) {
     return new Promise((resolve, reject) => {
@@ -19,13 +20,23 @@ function assets(cb) {
 
 function markup(cb) {
     return gulp.src('src/views/*.+(html|njk|nunjucks)')
-  .pipe(nunjucks({
+    .pipe(nunjucks({
       path: ['src/views/templates']
     }))
-  .pipe(gulp.dest('public/build/app'))
+    .pipe(gulp.dest('public/build/app'))
+}
+
+function makeSitemap(cb)
+{
+    return gulp.src('public/build/app/*.html', {
+        read: false
+    })
+    .pipe(sitemap({siteUrl: 'https://rowanpartnerships.online'}))
+    .pipe(gulp.dest('public/build/app'))
 }
 
 
 exports.assets = assets;
 exports.markup = markup;
-exports.default = gulp.series(markup, assets)
+exports.makeSitemap = makeSitemap;
+exports.default = gulp.series(markup, assets, makeSitemap)
